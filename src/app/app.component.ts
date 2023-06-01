b47dd32b7fc36a0b8621afc0b7ef33751dc4c835
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-06-01 15:55:39 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'theme-app';
+  darkThemeSelected =false ;
+  purpleThemeSelected=false;
+
+  switchTheme(event:any) {
+    console.log('called')
+    console.log(event)
+    if(event.value==='pink'){
+      this.darkThemeSelected =true ;
+      this.purpleThemeSelected =false ;
+    }else{
+      this.purpleThemeSelected =true ;
+      this.darkThemeSelected =false ;
+
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    FormsModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

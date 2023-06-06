a8949bb73b87f3d6415891c386e4c555c1174b43
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-06-06 10:57:17 +0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,31 +11,52 @@ export class AppComponent implements OnInit{
   defaultThemeSelected=false;
   pinkThemeSelected =false ;
   deepPurpleThemeSelected=false;
+  lightBg=false
+  darkBg=false
+  systemBg=false
 
 
   ngOnInit(): void {
+    this.initTheme();
+  }
+
+  initTheme(){
     let currentTheme = localStorage.getItem('currentTheme');
     console.log('====currentTheme', currentTheme)
 
     if(!currentTheme && window.matchMedia('(prefers-color-scheme: dark)').matches){
       console.log('dark')
+      this.systemBg=true ;
+      this.darkBg=false;
+      this.lightBg=false;
+
       this.pinkThemeSelected =true ;
       this.deepPurpleThemeSelected=false;
     }else{
+      this.systemBg=true ;
+      this.darkBg=false;
+      this.lightBg=false;
       console.log('light')
       this.deepPurpleThemeSelected=true;
       this.pinkThemeSelected =false ;
     }
 
     if( currentTheme === 'dark'){
+      this.systemBg=false ;
+      this.darkBg=true;
+      this.lightBg=false;
       this.pinkThemeSelected =true ;
       this.deepPurpleThemeSelected=false;
     }
 
     if( currentTheme === 'light'){
+      this.systemBg=false ;
+      this.darkBg=false;
+      this.lightBg=true;
       this.deepPurpleThemeSelected=true;
       this.pinkThemeSelected =false ;
     }
+
 
   }
 
@@ -47,20 +68,32 @@ export class AppComponent implements OnInit{
     if(event ==='system' ){
 
       if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        this.systemBg=true ;
+        this.darkBg=false;
+        this.lightBg=false;
         this.pinkThemeSelected =true ;
         this.deepPurpleThemeSelected=false;
       }else{
+        this.systemBg=true ;
+        this.darkBg=false;
+        this.lightBg=false;
         this.deepPurpleThemeSelected=true;
         this.pinkThemeSelected =false ;
       }
       }
 
     if(event ==='dark' ){
+      this.systemBg=false ;
+      this.darkBg=true;
+      this.lightBg=false;
       this.pinkThemeSelected =true ;
       this.deepPurpleThemeSelected=false;
     }
 
     if(event ==='light' ){
+      this.systemBg=false ;
+      this.darkBg=false;
+      this.lightBg=true;
       this.deepPurpleThemeSelected=true;
       this.pinkThemeSelected =false ;
     }
